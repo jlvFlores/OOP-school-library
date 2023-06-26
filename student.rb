298a2ed7621b38d8222c991d3e1d60f686180dca
jlvFlores <jluisvazquez.jv@gmail.com>
2023-06-26 12:23:26 -0600

    Add student class

---
+++ student.rb
@@ -0,0 +1,15 @@
+require './person.rb'
+
+class Student < Person
+  def initialize(classroom, age, name = "Unknown", parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
+
+paul = Student.new("6B", 16, "Paul")
+puts paul.play_hooky()
