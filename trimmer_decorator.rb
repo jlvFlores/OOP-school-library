f76a3c9e0621b6346aeeed995653ee0ee4eeb9ee
jlvFlores <jluisvazquez.jv@gmail.com>
2023-06-27 15:24:30 -0600

    Add decorators

---
+++ trimmer_decorator.rb
@@ -0,0 +1,8 @@
+require './base_decorator'
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    trimmed_name = @nameable.correct_name[0, 10]
+    trimmed_name.length < @nameable.correct_name.length ? trimmed_name.to_s : trimmed_name
+  end
+end
