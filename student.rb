ba60e938d38a2adaaaad8b95020d05eccef3a6cf
jlvFlores <jluisvazquez.jv@gmail.com>
2023-06-28 10:28:35 -0600

    Add Classroom class and relationships

--- student.rb
+++ student.rb
@@ -1,9 +1,12 @@
 require './person'
 
 class Student < Person
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+  attr_accessor :classroom
+
+  def initialize(age, name = 'Unknown', classroom = 'Not yet assigned', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    classroom.students << self if classroom != 'Not yet assigned'
   end
 
   def play_hooky
