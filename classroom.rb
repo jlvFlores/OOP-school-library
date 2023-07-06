851af88e21955cf3af5230f82c0e09604638865f
Cristian Zambrano <cristian@zamcham.com>
2023-07-06 10:26:58 -0400

    Update classroom.rb

--- classroom.rb
+++ classroom.rb
@@ -15,24 +15,3 @@ class Classroom
   end
 end
 
-class_1a = Classroom.new('Class 1-A')
-midoriya = Student.new(16, 'Midoriya')
-bakugo = Student.new(16, 'Bakugo', class_1a)
-
-puts midoriya.classroom
-puts bakugo.classroom
-# => Not yet assigned
-# => #<Classroom:0x00007ff659002970>
-
-puts class_1a.students
-# => #<Student:0x00007fed608d2778>
-
-class_1a.add_student(midoriya)
-puts midoriya.classroom
-puts midoriya.classroom.label
-# => #<Classroom:0x00007ff659002970>
-# => Class 1-A
-
-puts class_1a.students
-# => #<Student:0x00007fed608d2778>
-# => #<Student:0x00007fed608d2868>
